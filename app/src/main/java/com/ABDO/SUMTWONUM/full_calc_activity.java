31c875c8d81efd16cce737f16e79723e19c67884
abdomeshak <59654215+abdomeshak@users.noreply.github.com>
2020-03-01 00:43:33 +0200

    repair some bugs

--- app/src/main/java/com/ABDO/SUMTWONUM/full_calc_activity.java
+++ app/src/main/java/com/ABDO/SUMTWONUM/full_calc_activity.java
@@ -11,13 +11,13 @@ public class full_calc_activity extends AppCompatActivity {
     EditText all ;
     char opreation ;
     double result ;
-    double  num1 ;
-    double  num2 ;
+    String  num1 ;
+    String  num2 ;
 
      void ope (char x )
 
     {
-        num1    =Double.parseDouble(all.getText().toString());
+        num1    =all.getText().toString();
         all.setText("  ");
         opreation = x;
     }
@@ -34,7 +34,6 @@ public class full_calc_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.full_calc);
-
         Button zero=  findViewById(R.id.btn_0);
         Button one=  findViewById(R.id.btn_1);
         Button two=  findViewById(R.id.btn_2);
@@ -58,169 +57,191 @@ public class full_calc_activity extends AppCompatActivity {
 
 
 
+try {
+    pls.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ope('+');
+        }
+    });
+    mins.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ope('-');
+        }
+    });
+    mult.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ope('*');
+        }
+    });
+    div.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ope('/');
+        }
+    });
+    dot.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            all.setText(".");
+        }
+    });
+} catch (Exception e) {
+    e.printStackTrace();
+}
 
-        pls.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ope('+');
-            }
-        });
-        mins.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View v) {
-                   ope('-');
-               }
-           });
-        mult.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ope('*');
-            }
-        });
-        div.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ope('/');
-            }
-        });
-        dot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                all.setText(".");
-            }
-        });
-
-
-
-        zero.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                num("0");
+try {
 
 
-            }
-        });
-        one.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    zero.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
-                num("1");
-
-
-            }
-        }); two.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                num("2");
-
-
-            }
-        }); three.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                num("3");
-
-
-            }
-        }); four.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                num("4");
-
-
-            }
-        }); five.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                num("5");
-
-
-            }
-        }); six.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-               num("6");
-
-
-            }
-        }); seven.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-               num("7");
-
-
-            }
-        }); eight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                num("8");
-
-
-            }
-        }); nine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                num("9");
-
-
-            }
-        });
-        clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                all.setText(" ");
-
-
-            }
-        });
-        exit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-              System.exit(0);
-
-            }
-        });
-        equal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                num2    =Double.parseDouble(all.getText().toString());
-                switch (opreation)
-                {
-                    case '+': result = num1 + num2; break;
-                    case '-': result = num1 - num2; break;
-                    case '*': result = num1 * num2; break;
-                    case '/': result = num1 / num2; break;
-                }
-                all.setText(String.valueOf(result));
-            }
+            num("0");
 
 
         }
-        );
+    });
+    one.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
-        mins_mins.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            num("1");
 
-                double num1    =-1*Double.parseDouble(all.getText().toString());
 
-                String xx = String.valueOf(num1);
-                all.setText(xx);
+        }
+    });
+    two.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
-            }
-        });
+            num("2");
+
+
+        }
+    });
+    three.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            num("3");
+
+
+        }
+    });
+    four.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            num("4");
+
+
+        }
+    });
+    five.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            num("5");
+
+
+        }
+    });
+    six.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            num("6");
+
+
+        }
+    });
+    seven.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            num("7");
+
+
+        }
+    });
+    eight.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            num("8");
+
+
+        }
+    });
+    nine.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            num("9");
+
+
+        }
+    });
+    clear.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            all.setText(" ");
+
+
+        }
+    });
+    exit.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            System.exit(0);
+
+        }
+    });
+    equal.setOnClickListener(new View.OnClickListener() {
+                                 @Override
+                                 public void onClick(View v) {
+
+                                     num2 = all.getText().toString();
+                                     switch (opreation) {
+                                         case '+':
+                                             result = Double.parseDouble(num1 )+ Double.parseDouble(num2);
+                                             break;
+                                         case '-':
+                                             result =  Double.parseDouble(num1 )- Double.parseDouble(num2);
+                                             break;
+                                         case '*':
+                                             result =  Double.parseDouble(num1 )* Double.parseDouble(num2);
+                                             break;
+                                         case '/':
+                                             result =  Double.parseDouble(num1 )/ Double.parseDouble(num2);
+                                             break;
+                                     }
+                                     all.setText(String.valueOf(result));
+                                 }
+
+
+                             }
+    );
+
+    mins_mins.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            String num1 = String.valueOf(-1 * Double.parseDouble(all.getText().toString()));
+
+            String xx = String.valueOf(num1);
+            all.setText(xx);
+
+        }
+    });
+} catch (Exception e) {
+    e.printStackTrace();
+}
 
     }
 }
